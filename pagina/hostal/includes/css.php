33fefbc083af4b50c77e4c2035f49d87591fd3df
Evves <elvis.munoz02@inacpmail.cl>
2014-10-15 20:50:02 -0300

    pagina
    se agrego pagina y se pasaron imagenes a carpeta especificas

---
+++ pagina/hostal/includes/css.php
@@ -0,0 +1,7 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- theme for bootstrap -->
+
+<!-- mi estilo css -->
+<link rel="stylesheet" href="./css/style.css">
